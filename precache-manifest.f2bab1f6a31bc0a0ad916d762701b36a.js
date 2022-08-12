46ef82c2dc3211d152f5b309aad67db547dc3090
Jignesh Patel <jigpatel@jigpatel-mbp.jnpr.net>
2022-08-12 12:25:42 -0400

    Updates

---
+++ precache-manifest.f2bab1f6a31bc0a0ad916d762701b36a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bb5411728a556e992c0",
+    "url": "/my-app/static/css/main.d69ce17c.chunk.css"
+  },
+  {
+    "revision": "4bb5411728a556e992c0",
+    "url": "/my-app/static/js/main.4bb54117.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/my-app/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "043b86fed264a3f1abc6",
+    "url": "/my-app/static/js/2.043b86fe.chunk.js"
+  },
+  {
+    "revision": "e8b56c0b1af39a8bbb4e",
+    "url": "/my-app/static/js/runtime~main.e8b56c0b.js"
+  },
+  {
+    "revision": "0db3a3eff04d33a3e2db3f86b207d71f",
+    "url": "/my-app/index.html"
+  }
+];
